5bce1f4e284e61acf6a9ccfd308ea27d3e025352
bluesDD <40015780+bluesDD@users.noreply.github.com>
2018-06-13 21:24:55 +0900

    Add files via upload

---
+++ bitticker.py
@@ -0,0 +1,14 @@
+﻿# -*- coding: utf-8 -*-
+
+from pprint import pprint
+import pybitflyer
+pub_api = pybitflyer.API()
+
+#Ticker情報の取得
+def get_ticker():
+    ticker = pub_api.ticker(product_code='FX_BTC_JPY')
+    pprint(ticker)
+
+if __name__ == '__main__':
+    get_ticker()
+    
